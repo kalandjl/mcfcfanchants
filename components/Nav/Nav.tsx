3b19de7000d04d7d4dc15133a420b30b3ecea3aa
Kalan Jarvis-Loewen <karj5903@gmail.com>
2023-12-25 13:09:39 -0800

    Fixed mapping key error

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -17,6 +17,7 @@ const Nav: FC<Props> = (props: Props) => {
                 className="flex text-white pl-14 py-3 font-bold text-lg">
                     {links.map((link, i) => (
                     <p
+                    key={`link-${i}`}
                     className={`${i === 0 ? "" : "border-l-2 pl-7 ml-7 border-gray-200"} hover:underline hover:cursor-pointer underline-offset-2`}>
                         {link}
                     </p>
