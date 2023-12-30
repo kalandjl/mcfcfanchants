5df4ebb57756c8f6fced7a62fe565bee0ae67334
Kalan Jarvs-Loewen <karj5903@gmail.com>
2023-12-29 19:18:53 -0800

    Firestore suggestions working

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,20 @@
-import { Timestamp, addDoc, collection } from "firebase/firestore"
+import { Timestamp, addDoc, collection, getDocs, query } from "firebase/firestore"
 import { db } from "./firebase"
 
 export const addChant = async (email: string, title: string, lyrics: string) => {
 
     try {
-        const docRef = await addDoc(collection(db, "suggestions"), {
+        
+        console.log(email, title, lyrics)
+
+        const docRef = await addDoc(collection(db, "Submissions"), {
             email: email,
             title: title, 
             lyrics: lyrics,
             date: Timestamp.now()
         })
         
+        console.log(docRef)
         window.alert("Suggestion sent!")
     } catch (e) {
         window.Error("Error sending suggestion, " + e)
