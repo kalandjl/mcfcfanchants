8353aecc56a785452d2905cf4f2ecf0d01e6bcbb
Kalan Jarvs-Loewen <karj5903@gmail.com>
2023-12-31 20:29:21 -0800

    env variables updated

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 const nextConfig = {
     images: { unoptimized: true },
     env: {
-        "API_KEY":"AIzaSyByrcUgER6GSSg-fdnrTwZgYhXKk9LNpt4",
-        "AUTH_DOMAIN":"mcfcfanchants-2fece.firebaseapp.com",
-        "PROJECT_ID":"mcfcfanchants-2fece",
-        "STORAGE_BUCKET":"mcfcfanchants-2fece.appspot.com",
-        "MESSAGING_SENDER_ID":"1043257526255",
-        "APP_ID":"1:1043257526255:web:a3277c769c7165298c981a",
-        "MEASUREMENT_ID":"G-7EKHDTGQYB"
+        "NEXT_PUBLIC_API_KEY":"AIzaSyByrcUgER6GSSg-fdnrTwZgYhXKk9LNpt4",
+        "NEXT_PUBLIC_AUTH_DOMAIN":"mcfcfanchants-2fece.firebaseapp.com",
+        "NEXT_PUBLIC_PROJECT_ID":"mcfcfanchants-2fece",
+        "NEXT_PUBLIC_STORAGE_BUCKET":"mcfcfanchants-2fece.appspot.com",
+        "NEXT_PUBLIC_MESSAGING_SENDER_ID":"1043257526255",
+        "NEXT_PUBLIC_APP_ID":"1:1043257526255:web:a3277c769c7165298c981a",
+        "NEXT_PUBLIC_MEASUREMENT_ID":"G-7EKHDTGQYB"
     }
 }
 
