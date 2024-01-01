c30fbd8b0ecfe10278a44786e31d95fd0b9ceccc
Kalan Jarvs-Loewen <karj5903@gmail.com>
2023-12-31 17:55:30 -0800

    Config

---
+++ config/firebase.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyByrcUgER6GSSg-fdnrTwZgYhXKk9LNpt4",
+    authDomain: "mcfcfanchants-2fece.firebaseapp.com",
+    projectId: "mcfcfanchants-2fece",
+    storageBucket: "mcfcfanchants-2fece.appspot.com",
+    messagingSenderId: "1043257526255",
+    appId: "1:1043257526255:web:a3277c769c7165298c981a",
+    measurementId: "G-7EKHDTGQYB"
+  };
+  
