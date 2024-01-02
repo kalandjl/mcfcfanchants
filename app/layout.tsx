72d90a89fd73b777285078e4838443c901052809
Kalan Jarvs-Loewen <karj5903@gmail.com>
2024-01-01 21:47:52 -0800

    Icon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,14 @@ export default function RootLayout({
 
     return (
         <html lang="en">
+            <head>
+                <link
+                rel="icon"
+                href="/icon?<generated>"
+                type="image/<generated>"
+                sizes="<generated>"
+                />
+            </head>
             <body
             className={`${inter.className} bg-gray-100`}>
                 <section
