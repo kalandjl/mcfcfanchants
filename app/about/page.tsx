ad7cc3c290f8a2d7ebee064d21958b1861ddbeb4
Kalan Jarvs-Loewen <karj5903@gmail.com>
2024-01-11 23:50:06 -0800

    Updated about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,27 +7,23 @@ const Home = () => {
     return (
         <>
             <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
-minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
-quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
-fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
-consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-doloremque. Quaerat provident commodi consectetur veniam similique ad 
-earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
-fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
-suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
-modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
-totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
-quasi aliquam eligendi, placeat qui corporis!
+                {`     As a Man City fan, I noticed that on the internet there weren't 
+                any dedicated chant websites for the club where you could search 
+                and find chants based of the player, subject, or theme. Along 
+                with this, it had been a while since I put my programming skills to
+                use and had never created an project that was publicly avaliable.
+                So I decided to create MCFC Fan Chants as a way of combining my 
+                love of football with my love of coding, and also a reliable source
+                that Citizens anywhere could use if they wanted to learn new chants,
+                find lyrics, or just listen to the classics. I've spent a lot of time 
+                to deliver this project, fully made by myself, and I hope that everyone
+                can get something out of the website as it was intended to do so. 
+                I'd also like to thank anyone reading this for checking the website 
+                out, it means a lot to me and if you have any questions, suggestions
+                or comments you can find me on the contact page.`}
+                <br></br>
+                <br></br>
+                Thanks, Kalan
             </p>
         </>
     )
--- lib/contact.ts
+++ lib/contact.ts
@@ -2,5 +2,6 @@
 export const contacts = [
     "mcfcfanchants@gmail.com",
     "604-710-0331",
-    "Insta: kalandjl"
+    "Insta: kalandjl",
+    "github.com/kalandjl/mcfcfanchants"
 ]
